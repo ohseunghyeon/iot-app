4aa7150648cac6dde99660627a3aebb5fcb7da47
Oh SeungHyeon <postercolo@gmail.com>
2021-11-12 19:12:27 +0900

    Update

--- Screen/DrawerScreens/DeviceRegister.tsx
+++ Screen/DrawerScreens/DeviceRegister.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { manager } from '../../util/bleManager';
+
 import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function DeviceRegister() {
+  const scanAndConnect = () => {
+    manager.startDeviceScan(null, null, (error, device) => {
+      console.log('error', error, 'device', device?.id, device?.name, device?.serviceUUIDs);
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        return
+      }
 
+      // Check if it is a device you are looking for based on advertisement data
+      // or other criteria.
+      if (device.name === 'TI BLE Sensor Tag' ||
+        device.name === 'SensorTag') {
+
+        // Stop scanning as it's not necessary if you are scanning for one device.
+        manager.stopDeviceScan();
+
+        // Proceed with connection.
+      }
+    });
+  };
+
+  useEffect(() => {
+    console.log('start scanning');
+    scanAndConnect();
+  }, []);
   // 블루투스 모듈로 기기 찾기
 
   // 그리고 리스트 뿌려주기
